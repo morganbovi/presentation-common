e21be3982f61c607a2790ddd8740e8df8454f15e
bovi <morgan.bovi@gmail.com>
2021-11-24 16:48:10 -0600

    Anything

--- src/main/java/com/apkrocket/presentation_common/BaseActivity.kt
+++ src/main/java/com/apkrocket/presentation_common/BaseActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 
 @SuppressLint("Registered")
 abstract class BaseActivity : Activity() {
+    //Anything
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
